5228fb65d9c416256d72a138468802d6876e694d
t112360140 <155874754+t112360140@users.noreply.github.com>
2024-05-08 00:19:55 +0800

    Add files via upload

---
+++ Snake AI/Snake.js
@@ -0,0 +1,280 @@
+class Snake{
+
+    height=20;
+    width=20;
+    blockW=20;
+    map=[];
+    face=0;
+    pos=[];
+    score=0;
+    MaxScore=0;
+    long=1;
+    isStart=false;
+    speed=200;
+
+    print=true;
+    
+    constructor(element,blockWidht,x,y){
+        this.blockW=blockWidht;
+        (x>0)?this.width=x:this.width=20;
+        (y>0)?this.height=y:this.height=20;
+        let scoreboard=document.createElement("div");
+        this.scoreboard=element.appendChild(scoreboard);
+        let canvas=document.createElement("canvas");
+        canvas.height=(this.height+1)*this.blockW;
+        canvas.width=(this.width+5)*this.blockW;
+        canvas.getContext("2d").fillStyle="#000000";
+        canvas.getContext("2d").fillRect(0,0,(this.width)*this.blockW,(this.height)*this.blockW)
+        this.canvas=element.appendChild(canvas);
+        this.paint();
+        this.reset();
+    }
+
+    reset(){
+        for(let i=0;i<this.height*this.width;i++){
+            this.map[i]=0;
+        }
+        this.score=0;
+        this.face=0;
+        this.lastScore=0;
+        this.pos=[getRandom((this.width/4-1),(this.width*3/4-1)),getRandom((this.height/4-1),(this.height*3/4-1))];
+        this.map[this.pos[1]*this.width+this.pos[0]]=this.score+this.long;
+        this.setBerry();
+        this.paint();
+        const area=this.getArea();
+        return [(this.score+this.long),this.getdic(),area,(this.getSpaceArea()-area)];
+    }
+
+    start(nextAction){
+        if(this.isStart){
+            this.isStart=false;
+            clearInterval(this.loop);
+        }else{
+            this.isStart=true;
+            this.loop=setInterval(()=>{
+                if(!this.next(nextAction)){
+                    if(this.score>this.MaxScore){
+                        this.MaxScore=this.score;
+                    }
+                    this.isStart=false;
+                    clearInterval(this.loop);
+                    alert('Game over!\nScore:'+this.score);
+                    this.reset();
+                }
+                this.paint();
+            },this.speed);
+        }
+    }
+
+    next(nextAction){
+        if(nextAction!=null){
+            let action=nextAction.nextAction(this.getNextStatus());
+            if(action==null){
+                return false;
+            }
+            this.setFace(action);
+        }
+        for(let i=0;i<this.height*this.width;i++){
+            if(this.map[i]>0){
+                this.map[i]--;
+            }
+        }
+        switch(this.face){
+            case 0:{
+                if(this.pos[1]<=0||this.map[(this.pos[1]-1)*this.width+this.pos[0]]>0){
+                    return false;
+                }
+                this.pos[1]--;
+                break;
+            }
+            case 1:{
+                if(this.pos[0]>this.width-2||this.map[this.pos[1]*this.width+(this.pos[0]+1)]>0){
+                    return false;
+                }
+                this.pos[0]++;
+                break;
+            }
+            case 2:{
+                if(this.pos[1]>this.height-2||this.map[(this.pos[1]+1)*this.width+this.pos[0]]>0){
+                    return false;
+                }
+                this.pos[1]++;
+                break;
+            }
+            case 3:{
+                if(this.pos[0]<=0||this.map[this.pos[1]*this.width+(this.pos[0]-1)]>0){
+                    return false;
+                }
+                this.pos[0]--;
+                break;
+            }
+        }
+        if(this.map[this.pos[1]*this.width+this.pos[0]]<0){
+            this.score++;
+            if(!this.setBerry()){
+                return false;
+            }
+        }
+        this.map[this.pos[1]*this.width+this.pos[0]]=this.score+this.long;
+        return true;
+    }
+
+    setFace(face){
+        if(face!=((this.face+2)%4)){
+            this.face=face;
+            return true;
+        }
+        return false;
+    }
+
+    setBerry(){
+        let space=false;
+        for(let i=0;i<this.height*this.width;i++){
+            if(this.map[i]==0){
+                space=true;
+            }
+        }
+        while(space){
+            let Bpos=[getRandom(0,this.width-1),getRandom(0,this.width-1)];
+            if(this.map[Bpos[1]*this.width+Bpos[0]]==0){
+                this.map[Bpos[1]*this.width+Bpos[0]]=-1;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    async paint(){
+        if(this.canvas.getContext&&this.print){
+            let ctx=this.canvas.getContext("2d");
+            ctx.fillStyle="#000000";
+            ctx.fillRect(0,0,this.width*this.blockW,this.height*this.blockW);
+            for(let i=0;i<this.height;i++){
+                for(let j=0;j<this.width;j++){
+                    if(this.map[i*this.width+j]<0){
+                        ctx.fillStyle="#ff0000";
+                        ctx.fillRect((j+0.1)*this.blockW,(i+0.1)*this.blockW,this.blockW*0.8,this.blockW*0.8);
+                    }else if(this.map[i*this.width+j]>0){
+                        if(this.map[i*this.width+j]==(this.score+this.long)){
+                            ctx.fillStyle="#ffff00";
+                        }else{
+                            ctx.fillStyle="#00ff00";
+                        }
+                        ctx.fillRect((j+0.05)*this.blockW,(i+0.05)*this.blockW,this.blockW*0.9,this.blockW*0.9);
+                    }
+                }
+            }
+        }
+        
+        this.scoreboard.innerHTML="Score:"+this.score+"&nbsp;Highest Score:"+this.MaxScore;
+        
+    }
+
+    getArea(map,pos){
+        let tMap;
+        let tPos;
+        let area=0;
+        map?tMap=map:tMap=cloneJSON(this.map);
+        pos?tPos=pos:tPos=cloneJSON(this.pos);
+        tMap[tPos[1]*this.width+tPos[0]]=-2;
+        if(tPos[1]>0&&tMap[(tPos[1]-1)*this.width+tPos[0]]==0){
+            area+=(1+this.getArea(tMap,[tPos[0],tPos[1]-1]));
+        }
+        if(tPos[0]<(this.width-1)&&tMap[tPos[1]*this.width+(tPos[0]+1)]==0){
+            area+=(1+this.getArea(tMap,[tPos[0]+1,tPos[1]]));
+        }
+        if(tPos[1]<(this.height-1)&&tMap[(tPos[1]+1)*this.width+tPos[0]]==0){
+            area+=(1+this.getArea(tMap,[tPos[0],tPos[1]+1]));
+        }
+        if(tPos[0]>0&&tMap[tPos[1]*this.width+(tPos[0]-1)]==0){
+            area+=(1+this.getArea(tMap,[tPos[0]-1,tPos[1]]));
+        }
+        return area;
+    }
+
+    getSpaceArea(){
+        let area=0;
+        for(let i=0;i<this.height*this.width;i++){
+            if(this.map[i]==0){
+                area++;
+            }
+        }
+        return area;
+    }
+
+    getdic(){
+        let Bpos=null;
+        for(let i=0;i<this.height;i++){
+            for(let j=0;j<this.width;j++){
+                if(this.map[i*this.width+j]<0){
+                    Bpos=[j,i];
+                    break;
+                }
+            }
+            if(Bpos!=null){
+                break;
+            }
+        }
+        return (Math.abs(Bpos[0]-this.pos[0])+Math.abs(Bpos[1]-this.pos[1]));
+    }
+
+    getNextStatus(){
+        let tMap=cloneJSON(this.map);
+        let tPos=cloneJSON(this.pos);
+        let tFace=this.face;
+        let tScore=this.score;
+        let lastScore=0;
+        let out=[];
+        for(let i=0;i<4;i++){
+            lastScore=this.score;
+            if(this.setFace(i)){
+                if(this.next()){
+                    const area=this.getArea();
+                    out.push([i,[(this.score+this.long),(this.score-lastScore)?0:this.getdic(),area,(this.getSpaceArea()-area)]]);
+                }
+            }
+            this.map=cloneJSON(tMap);
+            this.pos=cloneJSON(tPos);
+            this.face=tFace;
+            this.score=tScore;
+        }
+        return out;
+    }
+
+    play_game(action){
+        this.setFace(action);
+        if(this.next()){
+            const score=(this.score-this.lastScore);
+            this.lastScore=this.score;
+            return {"status":"Alive","score":score};
+        }else{
+            const score=(this.score-this.lastScore);
+            this.lastScore=this.score;
+            return {"status":"Game Over","score":score};
+        }
+    }
+    
+}
+
+function cloneJSON(obj){
+    if (obj===null||obj===undefined||typeof obj!=='object')  {
+        return obj
+    }
+    if (obj instanceof Array){
+        let cloneA=[];
+        for(let i=0;i<obj.length;i++) {
+            cloneA[i]=cloneJSON(obj[i]);
+        }              
+        return cloneA;
+    }
+    let cloneO={};   
+    for(let i in obj) {
+        cloneO[i]=cloneJSON(obj[i]);
+    }                  
+    return cloneO;
+}
+
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min+1) + min);
+}
---
+++ Snake AI/index.js
@@ -0,0 +1,443 @@
+
+var scores = [];
+var logs=[];
+var saveModel=false;
+var stopTrain=false;
+
+async function TrainModel(model,config){
+    let env = config["env"]!=null?config["env"]:Snake_list[0];
+    let episodes = config["episodes"]!=null?config["episodes"]:5000;
+    let max_steps = config["max_steps"]!=null?config["max_steps"]:5000;
+    let discount = config["discount"]!=null?config["discount"]:0.98;
+    let replay_mem_size = config["replay_mem_size"]!=null?config["replay_mem_size"]:20000;
+    let minibatch_size = config["minibatch_size"]!=null?config["minibatch_size"]:512;
+    let epsilon = config["epsilon"]!=null?config["epsilon"]:1;
+    let epsilon_min = config["epsilon_min"]!=null?config["epsilon_min"]:0;
+    let epsilon_stop_episode = config["epsilon_stop_episode"]!=null?config["epsilon_stop_episode"]:4500;
+    let learning_rate = config["learning_rate"]!=null?config["learning_rate"]:5e-3;
+    let epochs = config["epochs"]!=null?config["epochs"]:1;
+    let show_every = config["show_every"]!=null?config["show_every"]:50;
+    let replay_start_size = config["replay_start_size"]!=null?config["replay_start_size"]:2000;
+    let hidden_dims = config["hidden_dims"]!=null?config["hidden_dims"]:[64, 64];
+    let activations = config["activations"]!=null?config["activations"]:['relu', 'relu', 'linear'];
+
+    const min_score=config["min_score"]!=null?config["min_score"]:300;
+    const output_model=config["output_model"]!=null?config["output_model"]:null;
+    const BMax_add=config["BMax_add"]!=null?config["BMax_add"]:1;
+
+    let agent =new DQN(4, discount, replay_mem_size, minibatch_size, epsilon,epsilon_stop_episode, epsilon_min,  learning_rate,'meanSquaredError',tf.train.adam(learning_rate), hidden_dims, activations, replay_start_size, model);
+
+    scores = [];
+    logs=[];
+    let score=[0,0,0,Infinity];
+    
+    stopTrain=false;
+    saveModel=false;
+    let Bmax=config["Bmax"]!=null?config["Bmax"]:30;
+    
+    for(let episode=0;episode<episodes;episode++){
+        let current_state = env.reset();
+        env.isStart=true;
+        let done=false;
+        let step=0;
+        let Bstep=Bmax;
+        env.print=false;
+
+        if(show_every&&(episode%show_every)==0){
+            env.print=true;
+        }
+
+        while(!done&&(!max_steps||step<max_steps)){
+            const next_states=env.getNextStatus();
+            let temp=[];
+            for(let i=0;i<next_states.length;i++){
+                temp[i]=next_states[i][1];
+            }
+            if(temp[0]==undefined){
+                agent.update_replay_memory(current_state, 0, current_state, -50, true);
+                break;
+            }
+            const best_state=agent.best_state(temp);
+            let best_index=best_state[1];
+            let best_action = next_states[best_index][0];
+
+            const ret=env.play_game(best_action);
+            done=(ret["status"]=='Game Over');
+            let reward=(1*(Bstep/Bmax)+ret["score"]*10);
+            if(ret["score"]>0){
+                Bstep=Bmax;
+                Bmax+=BMax_add;
+            }
+            Bstep--;
+            if(done){
+                reward=-50;
+            }else if(Bstep<0){
+                done=true;
+                reward=-50;
+            }
+
+            if(env.print){
+                await env.paint();
+            }
+
+            agent.update_replay_memory(current_state, best_action, next_states[best_index][1], reward, done);
+            // await agent.train(current_state,next_states[best_index][1],reward,epochs);
+            current_state = next_states[best_index][1];
+            step++;
+        }
+        
+        score[0]++;
+        score[1]+=env.score;
+        if(env.score>score[2]){
+            score[2]=env.score;
+        }
+        if(env.score<score[3]){
+            score[3]=env.score;
+        }
+        if(env.print){
+            console.log("Episode "+episode+"  score: "+env.score+"  epsilon: "+Math.round(agent.epsilon*100)/100);
+            
+            console.log("Ave:"+(score[1]/score[0])+" Max:"+(score[2])+" Min:"+(score[3]));
+            score=[0,0,0,Infinity];
+        }
+
+        scores.push(env.score);
+        logs.push({"E":episode,"S":env.score,"e":Math.round(agent.epsilon*100)/100});
+
+        await agent.train(epochs);
+        // agent.setEpsilon();
+
+        if(env.score>=min_score||saveModel||(output_model&&episode==output_model)){
+            await agent.model.save("downloads://model");
+            saveModel=false;
+        }
+        if(stopTrain){
+            break;
+        }
+    }
+
+    await agent.model.save("downloads://model");
+}
+
+
+class DQN {
+    constructor(state_size = 4, discount = 0.98, replay_mem_size = 20000,
+        minibatch_size = 512, epsilon = 1,
+        epsilon_stop_episode = 1500, epsilon_min = 1e-3,
+        learning_rate = 1e-3, loss = 'meanSquaredError',
+        optimizer = tf.train.adam(1e-3), hidden_dims = [64, 64],
+        activations = ['relu', 'relu', 'linear'],
+        replay_start_size = null, model=null) {
+        this.state_size = state_size;
+        this.discount = discount;
+        this.memory = new ReplayMemory(replay_mem_size);
+        this.minibatch_size = minibatch_size;
+        this.epsilon = epsilon;
+        this.epsilon_min = epsilon_min;
+        this.epsilon_decay = (this.epsilon - this.epsilon_min) / epsilon_stop_episode;
+        this.learning_rate = learning_rate;
+        this.loss = loss;
+        this.optimizer = optimizer;
+        this.hidden_dims = hidden_dims;
+        this.activations = activations;
+        if(replay_start_size!=null){
+            replay_start_size = replay_mem_size / 2
+        }
+        this.replay_start_size = replay_start_size
+
+        if(model==null){
+            this.model = this.createModel();
+        }else{
+            this.model=model;
+        }
+    }
+
+    createModel() {
+        const model = tf.sequential();
+        model.add(tf.layers.dense({ units: this.hidden_dims[0], inputShape: this.state_size, activation: this.activations[0]}));
+        for (let i = 1; i < this.activations; i++) {
+            model.add(tf.layers.dense({ units: this.hidden_dims[i], activation: this.activations[i] }));
+        }
+        model.add(tf.layers.dense({ units: 1, activation: this.activations[this.activations.length - 1] }));
+
+        model.compile({optimizer:this.optimizer, loss:this.loss})
+
+        return model;
+    }
+
+    update_replay_memory(current_state, action, next_state, reward, done){
+        this.memory.append([current_state, action, next_state, reward, done]);
+    }
+
+    get_qs(state) {
+        return tf.tidy(() => {return this.model.predict(state).dataSync()[0];});
+    }
+
+    best_state(states) {
+        let max_value = null;
+        let best_state = null;
+
+        if (Math.random() <= this.epsilon) {
+            const i=getRandom(0,(states.length-1));
+            return [states[i],i];
+        }else{
+            let n=null
+            for(let i=0;i<states.length;i++){
+                const value=this.get_qs(tf.tidy(()=>{return tf.reshape(states[i],[1,this.state_size]);}));
+                if(max_value==null||max_value<value){
+                    max_value=value;
+                    best_state=states[i];
+                    n=i;
+                }
+            }
+            return [best_state,n];
+        }
+    }
+
+    async train(epochs=3){
+        if(this.memory.len()<this.replay_start_size||this.memory.len()<this.minibatch_size){
+            return;
+        }
+
+        let minibatch = this.memory.sample(this.minibatch_size);
+        let next_states=[];
+        for(let i=0;i<minibatch.length;i++){
+            next_states.push(minibatch[i][2]);
+        }
+
+        let temp=tf.tidy(() => {return this.model.predict(tf.tensor(next_states)).dataSync();});
+        let next_qs=[];
+        for(let i=0;i<temp.length;i++){
+            next_qs[i]=temp[i];
+        }
+
+        let x=[];
+        let y=[];
+
+        for(let i=0;i<minibatch.length;i++){
+            let new_q=minibatch[i][3];
+            if(!minibatch[i][4]){
+                new_q=minibatch[i][3]+this.discount*next_qs[i];
+            }
+
+            x.push(minibatch[i][0]);
+            y.push(new_q);
+        }
+
+        await this.model.fit(tf.tensor(x), tf.tensor(y), {batch_size:this.minibatch_size, epochs:epochs, verbose:0});
+
+        if (this.epsilon > this.epsilon_min){
+            this.epsilon -= this.epsilon_decay;
+        }
+    }
+}
+
+class ReplayMemory {
+    constructor(maxLen) {
+        this.maxLen = maxLen;
+        this.buffer = [];
+        for (let i = 0; i < maxLen; ++i) {
+            this.buffer.push(null);
+        }
+        this.index = 0;
+        this.length = 0;
+
+        this.bufferIndices_ = [];
+        for (let i = 0; i < maxLen; ++i) {
+            this.bufferIndices_.push(i);
+        }
+    }
+
+    append(item) {
+        this.buffer[this.index] = item;
+        this.length = Math.min(this.length + 1, this.maxLen);
+        this.index = (this.index + 1) % this.maxLen;
+    }
+
+    len(){
+        let num=0;
+        for(let i=0;i<this.maxLen;i++){
+            if(this.buffer[i]!=null){
+                num++;
+            }
+        }
+        return num;
+    }
+
+    sample(batchSize) {
+        if (batchSize > this.maxLen) {
+            throw new Error(
+                `batchSize (${batchSize}) exceeds buffer length (${this.maxLen})`);
+        }
+
+        tf.util.shuffle(this.bufferIndices_);
+
+        const out = [];
+        let i=0;
+        let n=0;
+        while(i < batchSize){
+            if(this.buffer[this.bufferIndices_[n]]!=null){
+                out.push(this.buffer[this.bufferIndices_[n]]);
+                i++;
+            }
+            n++;
+        }
+        return out;
+    }
+}
+
+/**
+class DQN {
+    constructor(state_size = 4, discount = 0.98, epsilon = 1,
+        epsilon_stop_episode = 1500, epsilon_min = 1e-3,
+        learning_rate = 1e-3, loss = 'meanSquaredError',
+        optimizer = tf.train.adam(1e-3), hidden_dims = [64, 64],
+        activations = ['relu', 'relu', 'linear'],model=null) {
+        this.state_size = state_size;
+        this.discount = discount;
+        this.epsilon = epsilon;
+        this.epsilon_min = epsilon_min;
+        this.epsilon_decay = (this.epsilon - this.epsilon_min) / epsilon_stop_episode;
+        this.learning_rate = learning_rate;
+        this.loss = loss;
+        this.optimizer = optimizer;
+        this.hidden_dims = hidden_dims;
+        this.activations = activations;
+
+        if(model==null){
+            this.model = this.createModel();
+        }else{
+            this.model=model;
+        }
+    }
+
+    createModel() {
+        const model = tf.sequential();
+        model.add(tf.layers.dense({ units: this.hidden_dims[0], inputShape: this.state_size, activation: this.activations[0]}));
+        for (let i = 1; i < this.activations; i++) {
+            model.add(tf.layers.dense({ units: this.hidden_dims[i], activation: this.activations[i] }));
+        }
+        model.add(tf.layers.dense({ units: 1, activation: this.activations[this.activations.length - 1] }));
+
+        model.compile({optimizer:this.optimizer, loss:this.loss});
+
+        return model;
+    }
+
+    get_qs(state) {
+        return tf.tidy(() => {return this.model.predict(state).dataSync()[0];});
+    }
+
+    best_state(states) {
+        let max_value = null;
+        let best_state = null;
+
+        if (Math.random() <= this.epsilon) {
+            const i=getRandom(0,(states.length-1));
+            return [states[i],i];
+        }else{
+            let n=null
+            for(let i=0;i<states.length;i++){
+                const value=this.get_qs(tf.tidy(()=>{return tf.reshape(states[i],[1,this.state_size]);}));
+                if(max_value==null||max_value<value){
+                    max_value=value;
+                    best_state=states[i];
+                    n=i;
+                }
+            }
+            return [best_state,n];
+        }
+    }
+
+    async train(state,next_state,reward,epochs=3){
+
+        let temp=this.get_qs(tf.tidy(()=>{return tf.reshape(next_state,[1,this.state_size]);}));
+
+        let new_q=reward+this.discount*temp;
+        
+        await this.model.fit(tf.tensor([state]), tf.tensor([new_q]), {batch_size:1, epochs:epochs, verbose:0});
+    }
+
+    setEpsilon(){
+        if (this.epsilon > this.epsilon_min){
+            this.epsilon -= this.epsilon_decay;
+        }
+    }
+}
+**/
+
+class autoPlay{
+    
+    state_size=4;
+    epsilon=0;
+    useAI=false;
+    
+    constructor(game){
+        this.game=game;
+    }
+    
+    async loadModel(json,weights){
+        this.model=await tf.loadLayersModel(tf.io.browserFiles([json, weights]));
+    }
+    
+    async loadModelUrl(url){
+        this.model=await tf.loadLayersModel(url);
+    }
+    
+    get_qs(state) {
+        return tf.tidy(() => {return this.model.predict(state).dataSync()[0];});
+    }
+
+    best_state(states){
+        if(this.useAI){
+            let max_value = null;
+            let best_state = null;
+
+            if (Math.random() <= this.epsilon) {
+                const i=getRandom(0,(states.length-1));
+                return [states[i],i];
+            }else{
+                let n=null
+                for(let i=0;i<states.length;i++){
+                    const value=this.get_qs(tf.tidy(()=>{return tf.reshape(states[i],[1,this.state_size]);}));
+                    if(max_value==null||max_value<value){
+                        max_value=value;
+                        best_state=states[i];
+                        n=i;
+                    }
+                }
+                return [best_state,n];
+            }
+        }else{
+            let max_value=0;
+            let best_index=0;
+            for(let i=0;i<states.length;i++){
+                if(max_value<(states[i][2]-states[i][1]-states[i][3])){
+                    max_value=(states[i][2]-states[i][1]-states[i][3]);
+                    best_index=i;
+                }
+            }
+            return [null,best_index];
+        }
+
+    }
+
+    nextAction(states){
+        let temp=[];
+        for(let i=0;i<states.length;i++){
+            temp.push(states[i][1]);
+        }
+        
+        let bestState=this.best_state(temp);
+        if(states[bestState[1]]==undefined){
+            return null;
+        }else{
+            return (states[bestState[1]][0]);
+        }
+    }
+}
+
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min+1) + min);
+}
